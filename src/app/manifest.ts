b20a555208dd59145ba91501d3dd808fc9ca8671
HamaguchiRyota <52881627+HamaguchiRyota@users.noreply.github.com>
2025-02-08 15:50:10 +0900

    詳細を記入

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -2,9 +2,9 @@ import type { MetadataRoute } from "next";
 
 export default function manifest(): MetadataRoute.Manifest {
   return {
-    name: "Next.js App",
-    short_name: "Next.js App",
-    description: "Next.js App",
+    name: "ryota-space",
+    short_name: "ryota-space",
+    description: "ryota-spaceは、個人のポートフォリオサイトです。",
     start_url: "/",
     display: "standalone",
     background_color: "#fff",
