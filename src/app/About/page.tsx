fe70502fe48ae04f674ee2004a7cfb626f553d06
HamaguchiRyota <52881627+HamaguchiRyota@users.noreply.github.com>
2025-02-12 23:09:27 +0900

    「サイトについて」を追加。まだ不完全

---
+++ src/app/About/page.tsx
@@ -0,0 +1,113 @@
+import type { Metadata } from "next";
+import Image from "next/image";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import Link from "next/link";
+
+export const metadata: Metadata = {
+  title: "About｜ryota-space",
+  description: "本サイトの技術要件",
+  appleWebApp: true,
+};
+
+export default function about() {
+  return (
+    <div className="container mx-auto px-4 py-8">
+      <div className="pb-5">
+        <h1 className="text-3xl font-bold text-start mb-8 pb-3 border-b-2 border-zinc-700/10 dark:border-zinc-700">
+          本サイトについて
+        </h1>
+        <p className="text-lg mb-8">
+          本サイトは、個人のポートフォリオサイトです。
+          <br />
+        </p>
+        <div className="flex items-center justify-center space-x-4 mb-8 p-8">
+          <TooltipProvider delayDuration={100}>
+            <Tooltip>
+              <Link
+                href="https://nextjs.org/"
+                passHref
+                target="_blank"
+                rel="noreferrer"
+              >
+                <TooltipTrigger>
+                  <Image
+                    src="/nextjs-icon-light-background.svg"
+                    alt="Large avatar"
+                    width={180}
+                    height={180}
+                    className="dark:hidden"
+                  />
+                  <Image
+                    src="/nextjs-icon-dark-background.svg"
+                    alt="Large avatar"
+                    width={180}
+                    height={180}
+                    className="dark:block hidden"
+                  />
+                </TooltipTrigger>
+                <TooltipContent>
+                  <p>Next.js</p>
+                </TooltipContent>
+              </Link>
+            </Tooltip>
+          </TooltipProvider>
+          <TooltipProvider delayDuration={100}>
+            <Tooltip>
+              <Link
+                href="https://ui.shadcn.com/"
+                passHref
+                target="_blank"
+                rel="noreferrer"
+              >
+                <TooltipTrigger>
+                  <Image
+                    src="/shadcn-ui-seeklogo.svg"
+                    alt="Large avatar"
+                    width={180}
+                    height={180}
+                    className="dark:invert"
+                  />
+                </TooltipTrigger>
+                <TooltipContent>
+                  <p>shadcn</p>
+                </TooltipContent>
+              </Link>
+            </Tooltip>
+          </TooltipProvider>
+          <TooltipProvider delayDuration={100}>
+            <Tooltip>
+              <Link
+                href="https://vercel.com/"
+                passHref
+                target="_blank"
+                rel="noreferrer"
+              >
+                <TooltipTrigger>
+                  <Image
+                    src="/vercel-icon-dark.svg"
+                    alt="Large avatar"
+                    width={190}
+                    height={190}
+                    className="dark:invert"
+                  />
+                </TooltipTrigger>
+                <TooltipContent>
+                  <p>Vercel</p>
+                </TooltipContent>
+              </Link>
+            </Tooltip>
+          </TooltipProvider>
+        </div>
+
+        <p className="text-lg">
+          このサイトは、Next.jsとshadcnを使用して構築され、Vercelでホスティングされています。
+        </p>
+      </div>
+    </div>
+  );
+}
