23ff1fabff51f6288d987396a92c7f985a5180fd
HamaguchiRyota <52881627+HamaguchiRyota@users.noreply.github.com>
2025-03-06 22:39:01 +0900

    調整

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -46,7 +46,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-zinc-900`}
       >
         <ThemeProvider
           attribute="class"
