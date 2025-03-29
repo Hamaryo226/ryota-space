2cd6048bc6d8ed93c57cb04063c147816ec6ee2b
HamaguchiRyota <52881627+Hamaryo226@users.noreply.github.com>
2025-03-30 00:19:16 +0900

    使用しないURLはリダイレクトするように

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,21 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+
+  async redirects() {
+    return [
+      {
+        source: "/Project",
+        destination: "/",
+        permanent: false,
+      },
+      {
+        source: "/Private",
+        destination: "/",
+        permanent: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
